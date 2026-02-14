0aa772f66e6854cd0bdf70c92cb61e2a66a6404f
Happneel <happneelstunting@gmail.com>
2026-02-15 00:11:05 +0100

    [FIX] first test in frontend

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App'
 describe('App', () => {
   it('renders application title', () => {
     render(<App />)
-    expect(screen.getByText(/petite maison/i)).toBeInTheDocument()
+    expect(screen.getByText(/Espace communautaire de troc et/i)).toBeInTheDocument()
   })
 })
