e00776c330b3b4e3ce30f21f1ec6d4449b3c0166
Happneel <happneelstunting@gmail.com>
2026-02-15 00:23:13 +0100

    [FIX] add an import

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom/vitest'
 import { afterEach, beforeAll, afterAll, vi } from 'vitest'
 import { cleanup } from '@testing-library/react'
+import process from 'process'
 
 afterEach(() => cleanup())
 
